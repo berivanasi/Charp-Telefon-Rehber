05054874a246851013acfd2e5c04b700866436c7
Berivan <83500587+berivanasi@users.noreply.github.com>
2022-04-19 18:59:36 +0300

    Charp-Telefon-Rehber
    REHBER

---
+++ t_rehber/t_rehber/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace t_rehber
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dtg_rehber = new System.Windows.Forms.DataGridView();
+            this.btn_ekle = new System.Windows.Forms.Button();
+            this.btn_güncelle = new System.Windows.Forms.Button();
+            this.btn_sil = new System.Windows.Forms.Button();
+            this.lbl_numara = new System.Windows.Forms.Label();
+            this.lbl_isim = new System.Windows.Forms.Label();
+            this.txt_isim = new System.Windows.Forms.TextBox();
+            this.txt_soyad = new System.Windows.Forms.TextBox();
+            this.lbl_soyad = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.lbl_id = new System.Windows.Forms.Label();
+            this.txt_numara = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_rehber)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtg_rehber
+            // 
+            this.dtg_rehber.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtg_rehber.Location = new System.Drawing.Point(71, 242);
+            this.dtg_rehber.Name = "dtg_rehber";
+            this.dtg_rehber.RowTemplate.Height = 25;
+            this.dtg_rehber.Size = new System.Drawing.Size(392, 80);
+            this.dtg_rehber.TabIndex = 0;
+            // 
+            // btn_ekle
+            // 
+            this.btn_ekle.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.btn_ekle.Location = new System.Drawing.Point(236, 420);
+            this.btn_ekle.Name = "btn_ekle";
+            this.btn_ekle.Size = new System.Drawing.Size(75, 23);
+            this.btn_ekle.TabIndex = 1;
+            this.btn_ekle.Text = " Kişi Ekle";
+            this.btn_ekle.UseVisualStyleBackColor = true;
+            this.btn_ekle.Click += new System.EventHandler(this.btn_ekle_Click);
+            // 
+            // btn_güncelle
+            // 
+            this.btn_güncelle.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.btn_güncelle.Location = new System.Drawing.Point(309, 391);
+            this.btn_güncelle.Name = "btn_güncelle";
+            this.btn_güncelle.Size = new System.Drawing.Size(75, 23);
+            this.btn_güncelle.TabIndex = 2;
+            this.btn_güncelle.Text = "Güncelle";
+            this.btn_güncelle.UseVisualStyleBackColor = true;
+            this.btn_güncelle.Click += new System.EventHandler(this.btn_güncelle_Click);
+            // 
+            // btn_sil
+            // 
+            this.btn_sil.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.btn_sil.Location = new System.Drawing.Point(164, 392);
+            this.btn_sil.Name = "btn_sil";
+            this.btn_sil.Size = new System.Drawing.Size(75, 22);
+            this.btn_sil.TabIndex = 3;
+            this.btn_sil.Text = "Sil";
+            this.btn_sil.UseVisualStyleBackColor = true;
+            this.btn_sil.Click += new System.EventHandler(this.btn_sil_Click);
+            // 
+            // lbl_numara
+            // 
+            this.lbl_numara.AutoSize = true;
+            this.lbl_numara.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.lbl_numara.Location = new System.Drawing.Point(33, 122);
+            this.lbl_numara.Name = "lbl_numara";
+            this.lbl_numara.Size = new System.Drawing.Size(55, 17);
+            this.lbl_numara.TabIndex = 4;
+            this.lbl_numara.Text = "Numara";
+            // 
+            // lbl_isim
+            // 
+            this.lbl_isim.AutoSize = true;
+            this.lbl_isim.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.lbl_isim.Location = new System.Drawing.Point(38, 72);
+            this.lbl_isim.Name = "lbl_isim";
+            this.lbl_isim.Size = new System.Drawing.Size(30, 17);
+            this.lbl_isim.TabIndex = 6;
+            this.lbl_isim.Text = "İsim";
+            // 
+            // txt_isim
+            // 
+            this.txt_isim.Location = new System.Drawing.Point(105, 66);
+            this.txt_isim.Name = "txt_isim";
+            this.txt_isim.Size = new System.Drawing.Size(100, 23);
+            this.txt_isim.TabIndex = 7;
+            // 
+            // txt_soyad
+            // 
+            this.txt_soyad.Location = new System.Drawing.Point(105, 93);
+            this.txt_soyad.Name = "txt_soyad";
+            this.txt_soyad.Size = new System.Drawing.Size(100, 23);
+            this.txt_soyad.TabIndex = 8;
+            // 
+            // lbl_soyad
+            // 
+            this.lbl_soyad.AutoSize = true;
+            this.lbl_soyad.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.lbl_soyad.Location = new System.Drawing.Point(38, 96);
+            this.lbl_soyad.Name = "lbl_soyad";
+            this.lbl_soyad.Size = new System.Drawing.Size(41, 17);
+            this.lbl_soyad.TabIndex = 9;
+            this.lbl_soyad.Text = "Soyad";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_id);
+            this.groupBox1.Controls.Add(this.lbl_id);
+            this.groupBox1.Controls.Add(this.txt_numara);
+            this.groupBox1.Controls.Add(this.lbl_soyad);
+            this.groupBox1.Controls.Add(this.txt_soyad);
+            this.groupBox1.Controls.Add(this.txt_isim);
+            this.groupBox1.Controls.Add(this.lbl_isim);
+            this.groupBox1.Controls.Add(this.lbl_numara);
+            this.groupBox1.Location = new System.Drawing.Point(12, -2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(299, 165);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bilgiler";
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(105, 32);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(100, 23);
+            this.txt_id.TabIndex = 12;
+            // 
+            // lbl_id
+            // 
+            this.lbl_id.AutoSize = true;
+            this.lbl_id.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.lbl_id.Location = new System.Drawing.Point(49, 38);
+            this.lbl_id.Name = "lbl_id";
+            this.lbl_id.Size = new System.Drawing.Size(18, 17);
+            this.lbl_id.TabIndex = 11;
+            this.lbl_id.Text = "İd";
+            // 
+            // txt_numara
+            // 
+            this.txt_numara.Location = new System.Drawing.Point(105, 122);
+            this.txt_numara.Name = "txt_numara";
+            this.txt_numara.Size = new System.Drawing.Size(100, 23);
+            this.txt_numara.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(361, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(142, 135);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 328);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(128, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(526, 471);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_sil);
+            this.Controls.Add(this.btn_güncelle);
+            this.Controls.Add(this.btn_ekle);
+            this.Controls.Add(this.dtg_rehber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtg_rehber)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtg_rehber;
+        private System.Windows.Forms.Button btn_ekle;
+        private System.Windows.Forms.Button btn_güncelle;
+        private System.Windows.Forms.Button btn_sil;
+        private System.Windows.Forms.Label lbl_numara;
+        private System.Windows.Forms.Label lbl_isim;
+        private System.Windows.Forms.TextBox txt_isim;
+        private System.Windows.Forms.TextBox txt_soyad;
+        private System.Windows.Forms.Label lbl_soyad;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txt_numara;
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.Label lbl_id;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ t_rehber/t_rehber/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace t_rehber
+{
+    public partial class Form1 : Form
+
+    {
+         
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataSet ds;
+        void rehber()
+        {
+            con= new SqlConnection ("server=DESKTOP-VJBC22E;Initial Catalog=dbrehber;Integrated Security =True");
+            da = new SqlDataAdapter("Select * from telefon_rehberi ",con);
+            ds = new DataSet();
+            con.Open();
+            da.Fill(ds,"telefon_rehberi");
+            dtg_rehber.DataSource = ds.Tables["telefon_rehberi"];
+            con.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rehber();
+        }
+
+        private void btn_ekle_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "insert into telefon_rehberi (isim,soyad,numara) values ('" + txt_isim.Text + "','" + txt_soyad.Text + "','" + txt_numara.Text + "')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            rehber();
+        }
+
+        private void btn_sil_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "delete from telefon_rehberi where isim ='" + txt_isim.Text + "'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            rehber();
+        }
+
+        private void btn_güncelle_Click(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "update telefon_rehberi  set isim='" + txt_isim.Text + "', soyad='" + txt_soyad.Text + "', numara='" + txt_numara.Text + "' where id='" + txt_id.Text + "'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            rehber();
+        }
+    }
+}
